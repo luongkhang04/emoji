bba80e437d94aa9bbfb2df4bac35a81ae1092174
Lương Thái Khang <luongkhang448@gmail.com>
2026-02-02 23:35:23 +0700

    update icon

--- docs/sw.js
+++ docs/sw.js
@@ -6,11 +6,8 @@ const CORE_ASSETS = [
   "./main.js",
   "./manifest.webmanifest",
   "./icons/icon.svg",
-  "./icons/icon-maskable.svg",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
-  "./icons/icon-maskable-192.png",
-  "./icons/icon-maskable-512.png",
   "./screenshots/desktop.png",
   "./screenshots/mobile.png",
 ];
