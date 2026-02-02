06238d0eb62e74238d8f139ad9187b3a2bd49b61
Lương Thái Khang <luongkhang448@gmail.com>
2026-02-02 22:36:52 +0700

    add icons and screenshots for PWA manifest

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "emoji-composer-v1";
+const CACHE_NAME = "emoji-composer-v2";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -7,6 +7,12 @@ const CORE_ASSETS = [
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-192.png",
+  "./icons/icon-maskable-512.png",
+  "./screenshots/desktop.png",
+  "./screenshots/mobile.png",
 ];
 
 self.addEventListener("install", (event) => {
